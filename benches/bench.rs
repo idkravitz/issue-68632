e249590d6ea9dc17b8614c8b480cb937ccb142d6
Dmitry Kravtsov <idkravitz@gmail.com>
2020-01-30 00:05:38 +1000

    Remove crap from bench.rs

--- benches/bench.rs
+++ benches/bench.rs
@@ -5,7 +5,6 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 const N: usize = 1000000;
 
 fn benchmark_bad(c: &mut Criterion) {
-//    const N: usize = 1000000;
     c.bench_function(&format!("max_subarray([..]) N = {}", N), |b| {
         b.iter(|| max_subarray_bad(black_box(&vec![0; N])))
     });
@@ -13,7 +12,6 @@ fn benchmark_bad(c: &mut Criterion) {
 
 
 fn benchmark_good(c: &mut Criterion) {
-//    const N: usize = 1000000;
     c.bench_function(&format!("max_subarray([..]) N = {}", N), |b| {
         b.iter(|| max_subarray_good(black_box(&vec![0; N])))
     });
@@ -22,35 +20,3 @@ fn benchmark_good(c: &mut Criterion) {
 
 criterion_group!(benches, benchmark_bad, benchmark_good);
 criterion_main!(benches);
-
-// use criterion::{black_box, criterion_group, criterion_main, Criterion};
-
-// fn benchmark_recursive(c: &mut Criterion) {
-//     const N: usize = 20000;
-//     c.bench_function(&format!("max_subarray_recursive([.. N = {}])", N), |b| {
-//         b.iter(|| max_subarray::max_subarray_recursive(black_box(&vec![0; N])))
-//     });
-//     c.bench_function(&format!("max_subarray_recursive_old([.. N = {}])", N), |b| {
-//         b.iter(|| max_subarray::max_subarray_recursive_old(black_box(&vec![0; N])))
-//     });
-//     c.bench_function(&format!("max_subarray([.. N = {}])", N), |b| {
-//         b.iter(|| max_subarray::max_subarray(black_box(&vec![0; N])))
-//     });
-// }
-
-// fn benchmark_linear(c: &mut Criterion) {
-//     const N: usize = 1000000;
-//     c.bench_function(&format!("max_subarray([.. N = {}])", N), |b| {
-//         b.iter(|| max_subarray::max_subarray(black_box(&vec![0; N])))
-//     });
-//     c.bench_function(&format!("max_subarray2([.. N = {}])", N), |b| {
-//         b.iter(|| max_subarray::max_subarray2(black_box(&vec![0; N])))
-//     });
-//     c.bench_function(&format!("max_subarray3([.. N = {}])", N), |b| {
-//         b.iter(|| max_subarray::max_subarray3(black_box(&vec![0; N])))
-//     });
-// }
-
-
-// criterion_group!(benches/*, benchmark_recursive*/, benchmark_linear);
-// criterion_main!(benches);
